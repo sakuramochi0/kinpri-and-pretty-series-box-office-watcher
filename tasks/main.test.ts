9ccb0cdf3e7fc96dfe2d73f2722a836258a87d28
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-08-25 18:16:20 +0900

    feat: fetch latest posts for daily aggregation and write skeleton

--- tasks/main.test.ts
+++ tasks/main.test.ts
@@ -1,10 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://mimorin2014.com/archives.html');
-  await page.getByRole('link', { name: 'デイリー合算ランキング：20240824(土)', exact: true }).click();
-  await page.getByText('デイリー上映25分前販売数合計ランキング：20240824').click();
-  await page.getByText('デイリー合算ランキング：20240824(土) 2024/').click();
-  await page.getByRole('link', { name: '（独立系を含む）デイリー合算ランキング：20240824(土)', exact: true }).click();
-  await page.getByText('（独立系を含む）デイリー上映25分前販売数合計ランキング：').click();
+test('collect box office data from mimorin2014.com', async ({ page }) => {
+   const posts = await getPosts(page)
+  for (const post of posts) {
+    if (doesPostExists(post)) {
+      continue
+    }
+
+    savePost(post)
+  }
+  console.log(posts)
 });
+
+async function getPosts(page) {
+  await page.goto('https://mimorin2014.com/blog-category-6.html');
+  const posts = await page
+    .locator('.entry.list_content > .entry_body')
+    .evaluateAll(div => div
+      .filter(e => !e.textContent.match(/中間集計|25分前/))
+      .map(e => e.textContent.trim())
+    )
+  return posts;
+}
+
