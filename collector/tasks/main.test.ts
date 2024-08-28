918217b64a486a11a4a7f6b8aff3f1b92f649c04
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-08-28 23:28:53 +0900

    chore: auto format

--- collector/tasks/main.test.ts
+++ collector/tasks/main.test.ts
@@ -37,7 +37,7 @@ async function getPosts(page: Page, pageNumber: number = 0): Promise<Post[]> {
         date: new Date(e.querySelector('.entry_date').textContent.trim()),
         body: e.querySelector('.entry_body').innerHTML.trim().split('<br>'),
       }))
-      .filter(({title}) => title.match(/合算/))
-      .filter(({title}) => !title.match(/中間集計|25分前|前日集計/))
+      .filter(({ title }) => title.match(/合算/))
+      .filter(({ title }) => !title.match(/中間集計|25分前|前日集計/))
     )
 }
