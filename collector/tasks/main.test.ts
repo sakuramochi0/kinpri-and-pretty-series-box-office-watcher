19af6f7e927347626290e3f48cd778281de9d325
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-08-28 22:22:36 +0900

    fix: choose correct post for daily stats

--- collector/tasks/main.test.ts
+++ collector/tasks/main.test.ts
@@ -28,16 +28,16 @@ function savePost(filename: string, post: Post) {
 }
 
 async function getPosts(page: Page): Promise<Post[]> {
-  await page.goto('https://mimorin2014.com/blog-category-6.html');
+  await page.goto('https://mimorin2014.com/?cat=6&page=0');
   return await page
     .locator('.entry.list_content:has(> .entry_body)')
     .evaluateAll(div => div
-      .filter(e => !e.textContent.match(/中間集計|25分前/))
       .map(e => ({
         title: e.querySelector('.entry_header').textContent.trim(),
         date: new Date(e.querySelector('.entry_date').textContent.trim()),
         body: e.querySelector('.entry_body').textContent.trim(),
       }))
+      .filter(({title}) => title.match(/合算/))
+      .filter(({title}) => !title.match(/中間集計|25分前|前日集計/))
     )
 }
-
