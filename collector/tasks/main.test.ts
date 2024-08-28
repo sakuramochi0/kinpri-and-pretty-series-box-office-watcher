172aacbc736961c07723311a5dc1b28777674065
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-08-29 01:20:02 +0900

    refactor(collector): move data directory to /data/raw/

--- collector/tasks/main.test.ts
+++ collector/tasks/main.test.ts
@@ -20,7 +20,7 @@ test('collect box office data from mimorin2014.com', async ({ page }) => {
 });
 
 function getFilename(post: Post) {
-  return `../data/${post.title}.json`
+  return `../data/raw/${post.title}.json`
 }
 
 function savePost(filename: string, post: Post) {
