d509b4801fb8a179054d3e4abffdae3510ecfc35
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-09-01 19:41:45 +0900

    feat: filter out non pretty-series movies

--- collector/tasks/main.test.ts
+++ collector/tasks/main.test.ts
@@ -3,6 +3,10 @@ import { existsSync, writeFileSync } from 'node:fs'
 
 import { Post } from './types';
 
+const PRETTY_SERIES_TITLE_LIST = [
+  'KING OF PRISM',
+  'ＫＩＮＧ ＯＦ ＰＲＩＳＭ',
+]
 
 test('collect box office data from mimorin2014.com', async ({ page }) => {
   const posts = await getPosts(page)
@@ -30,7 +34,7 @@ function savePost(filename: string, post: Post) {
 
 async function getPosts(page: Page, pageNumber: number = 0): Promise<Post[]> {
   await page.goto(`https://mimorin2014.com/?cat=6&page=${pageNumber}`);
-  return await page
+  const posts = await page
     .locator('.entry.list_content:has(> .entry_body)')
     .evaluateAll(div => div
       .map(e => ({
@@ -42,4 +46,17 @@ async function getPosts(page: Page, pageNumber: number = 0): Promise<Post[]> {
       .filter(({ title }) => title.match(/合算/))
       .filter(({ title }) => !title.match(/中間集計|25分前|前日集計/))
     )
+  return posts.map(post => filterPrettySeriesTitle(post))
+}
+
+// filter out records other than pretty series title from body
+function filterPrettySeriesTitle(post: Post) {
+  const headers = post.body.slice(0, 2)
+  post.body = headers
+    .concat(
+      post.body.filter(
+      row => PRETTY_SERIES_TITLE_LIST.some(title => row.includes(title))
+    )
+  )
+  return post
 }
