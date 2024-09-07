ad47c57713ee6f8a2ded06134f4866bbc92bbc6e
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-09-08 04:53:40 +0900

    feat(visualizer): update description

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -9,7 +9,7 @@ type Record = typeof data.records[0]
 function App() {
   const headers = [
     '日付',
-    'ランク',
+    '順位',
     '販売座席数',
     '合計座席数',
     '上映回数',
@@ -25,12 +25,19 @@ function App() {
   return (
     <>
       <h1>『KING OF PRISM -Dramatic PRISM.1-』の販売座席数と興行収入</h1>
-      <p>このページは、<a href="https://mimorin2014.com/">興行収入を見守りたい！</a>さんが公開している座席販売数のデータをもとに、プリティーシリーズの映画の座席販売数・興行収入の変化を一覧できるようにしたページです。現在は『KING
-        OF PRISM -Dramatic PRISM.1-』に対応しています。</p>
-      <h2>座席販売数と先週比のグラフ</h2>
+      <p>
+        最終更新日時: {new Intl.DateTimeFormat('ja', {
+        dateStyle: 'long',
+        timeStyle: 'long'
+      }).format(new Date(data.updated + 'Z'))}
+      </p>
+      <p>
+        このページは、<a target="_blank" href="https://mimorin2014.com/">興行収入を見守りたい！</a>さんが公開している座席販売数のデータをもとに、<a target="_blank" href="https://www.takaratomy-arts.co.jp/specials/prettyseries/">プリティーシリーズ</a>の映画の座席販売数・興行収入の変化を表やグラフで確認できるようにしたページです。販売座席数のテーブルは1日1回自動更新されます。現在は『<a href="https://kinpri.com/">KING OF PRISM -Dramatic PRISM.1-</a>』に対応しています。
+      </p>
+      <h2>座席販売数と先週比のグラフ（仮）</h2>
       <iframe width="900" height="540"
               src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQK4EQdeuxlXz1Iy3RDWbAP0v1KYJDpFMWVGr6wguoPRl-9kMa5LA_ZaJcBM8uEHKKB1WLH38ZgpWOj/pubchart?oid=1244534495&format=interactive'></iframe>
-      <h2>座席販売数のデイリーランキングの一覧</h2>
+      <h2>座席販売数のテーブル</h2>
       <div className="card">
         <table>
           <thead>
@@ -44,17 +51,21 @@ function App() {
         </table>
       </div>
       <footer>
-        <p>
-          最終更新日時: {new Intl.DateTimeFormat('ja', {
-          dateStyle: 'long',
-          timeStyle: 'long'
-        }).format(new Date(data.updated + 'Z'))}
-        </p>
-        <p>ソースコード:
-          <a href="https://github.com/sakuramochi0/kinpri-and-pretty-series-box-office-watcher">
-            sakuramochi0/kinpri-and-pretty-series-box-office-watcher
-          </a>
-        </p>
+        <ul>
+          <li>
+            ソースコード:
+            <a href="https://github.com/sakuramochi0/kinpri-and-pretty-series-box-office-watcher">
+              sakuramochi0/kinpri-and-pretty-series-box-office-watcher
+            </a>
+          </li>
+          <li>
+            埋め込みグラフ（仮）:
+            <a
+              href="https://docs.google.com/spreadsheets/d/1fG0GpgVOnwaFCuwKsP_6qh06lW6V9Z_vcY0irF4Cw6s/edit?gid=1434128340#gid=1434128340">
+              『KING OF PRISM -Dramatic PRISM.1-』販売座席数 - Google スプレッドシート
+            </a>
+          </li>
+        </ul>
       </footer>
     </>
   )
