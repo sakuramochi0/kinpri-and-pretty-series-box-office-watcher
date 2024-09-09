53b014556907d33c1558795de1023d7e825d31cd
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-09-10 03:21:03 +0900

    style(visualizer): apply box style to latest dataset

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -39,17 +39,15 @@ function App() {
         href="https://kinpri.com/">KING OF PRISM -Dramatic PRISM.1-</a>』に対応しています。
       </p>
       <p>
-        <strong>注意：</strong>元データに当日ランキング上位25位までの映画しか掲載されないため、一部26位以下の日のデータが欠けています。これにより、推定興行収入やグラフが不完全な形になっています。
+        <strong>注意：</strong>元データに当日ランキング上位25位までの映画のデータしか掲載されないため、26位以下の日のデータが欠けていて、推定興行収入やグラフが不完全な形になっています。
       </p>
       <h2>最新データ</h2>
-      <ul>
-        <ul>
-          <li>日付：{formatDate(lastRecord.meta.record_date)}</li>
-          <li>販売座席数（先週比）：{lastRecord.record.sales}座席（{lastRecord.record.since_last_week}%）</li>
-          <li>累積販売数：{lastRecord.record.cumulative_sales?.toLocaleString()}座席</li>
-          <li>推定興行収入：{formatEstimatedBoxOffice(lastRecord.record.estimated_box_office)}</li>
-        </ul>
-      </ul>
+      <div id="latest-data">
+          <div>日付<br/>{formatDate(lastRecord.meta.record_date)}</div>
+          <div>販売座席数（先週比）<br/>{lastRecord.record.sales}座席（{lastRecord.record.since_last_week}%）</div>
+          <div>累積販売数<br/>{lastRecord.record.cumulative_sales?.toLocaleString()}座席</div>
+          <div>推定興行収入<br/>{formatEstimatedBoxOffice(lastRecord.record.estimated_box_office)}</div>
+      </div>
       <h2>座席販売数と先週比のグラフ（仮）</h2>
       <iframe width="900" height="540"
               src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQK4EQdeuxlXz1Iy3RDWbAP0v1KYJDpFMWVGr6wguoPRl-9kMa5LA_ZaJcBM8uEHKKB1WLH38ZgpWOj/pubchart?oid=1244534495&format=interactive'></iframe>
@@ -69,13 +67,13 @@ function App() {
       <footer>
         <ul>
           <li>
-            ソースコード:
+            ソースコード:{' '}
             <a href="https://github.com/sakuramochi0/kinpri-and-pretty-series-box-office-watcher">
               sakuramochi0/kinpri-and-pretty-series-box-office-watcher
             </a>
           </li>
           <li>
-            埋め込みグラフ（仮）:
+            埋め込みグラフ（仮）:{' '}
             <a
               href="https://docs.google.com/spreadsheets/d/1fG0GpgVOnwaFCuwKsP_6qh06lW6V9Z_vcY0irF4Cw6s/edit?gid=1434128340#gid=1434128340">
               『KING OF PRISM -Dramatic PRISM.1-』販売座席数 - Google スプレッドシート
